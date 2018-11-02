1dc871182da0f6b842213eb9b212be7688905d60
duciito <duciito@gmail.com>
2018-11-02 13:40:04 +0200

    create a separate menu module

--- sg/main.py
+++ sg/main.py
@@ -3,6 +3,8 @@
 import os
 import requests
 
+from menu import Menu
+
 # snooker_api = dict(
 #     event = requests.get('http://api.snooker.org/?e=398'),
 #     match = requests.get('http://api.snooker.org/?e=397&r=1&n=5'),
@@ -21,49 +23,7 @@ import requests
 #     print(f'{key}:   {value.json()}')
 
 
-class Menu:
-    """Model a simple menu that provides many options to the user."""
 
-    def __init__(self, options, name='generic menu'):
-        """Initialize the main options of the menu and set its name."""
-        # We pass options as a list and convert it to a dictionary starting from 1
-        self.name = name
-
-        options.append('Quit\n')
-        self.options = dict(enumerate(options, start=1))
-
-    def get_input(self):
-        """Get user input."""
-        answer = int(input('Please enter an option: ')) # the option is always a numerical value
-        return answer
-
-    def show_menu(self):
-        """List all options and a shiny ascii art title."""
-        print(f'{self.name}\n')
-
-        for number, option in self.options.items():
-            print(f"{number}. {option if type(option) is str else option['value']}")
-
-    def add_submenu(self, suboptions, option=1):
-        """Add a submenu with a main option provided."""
-        main_option = self.options[option]
-
-        suboptions.append('Go back')
-
-        # The top-level option is now a dictionary consisting of its value and its submenu.
-        self.options[option] = {
-            'value': main_option,
-            'submenu': dict(enumerate(suboptions, start=1))
-        }
-
-    def show_submenu(self, main_option):
-        """Show a submenu of the provided main option."""
-        if not self.options[main_option]['submenu']:
-            print('This option has no submenu.')
-            return
-
-        for number, option in self.options[main_option]['submenu'].items():
-            print(f"{number}. {option}")
 
 
 def show_info():
@@ -92,39 +52,26 @@ def determine_options():
 
     return tuple(options)  # options are already chosen and thus immutable
 
-def determine_action(options, funcs):
+def determine_action(options, dispatcher):
     """Perform a specific task based on user input."""
     main_option = options[0]
 
     if len(options) == 2:
         suboption = options[1]
+        action = dispatcher[main_option][suboption]
 
-        if suboption == list(funcs[main_option])[-1]:  # last option on submenus is go back
+        if suboption == list(dispatcher[main_option])[-1]:  # last option on submenus is go back
             os.system('clear')
 
             options = determine_options()
-            funcs[main_option][suboption](options, funcs)
+            action(options, dispatcher)
 
-        funcs[main_option][suboption]()
+        action()
+    else:
+        action = dispatcher[main_option]
+        action()
 
-    funcs[main_option]()
-
-
-
-# while True:
-#
-#
-#     if option not in menu.options.keys():
-#         print("\nThat's not a valid option!")
-#         break
-#     elif option == len(menu.options):  # last option is always quit
-#         break
-#     else:
-#         determine_action(option)  # to be implemented
-
-# store main funcs in a dict
-
-funcs = {
+dispatcher = {
     1: show_info,
     2: {
         1: show_info,
@@ -136,4 +83,4 @@ funcs = {
 }
 
 options = determine_options()
-determine_action(options, funcs)
+determine_action(options, dispatcher)
---
+++ sg/menu.py
@@ -0,0 +1,46 @@
+"""Constains a simple Menu class used for interacting with the user."""
+
+
+class Menu:
+    """Model a simple menu that provides many options to the user."""
+
+    def __init__(self, options, name='generic menu'):
+        """Initialize the main options of the menu and set its name."""
+        # We pass options as a list and convert it to a dictionary starting from 1
+        self.name = name
+
+        options.append('Quit\n')
+        self.options = dict(enumerate(options, start=1))
+
+    def get_input(self):
+        """Get user input."""
+        answer = int(input('Please enter an option: ')) # the option is always a numerical value
+        return answer
+
+    def show_menu(self):
+        """List all options and a shiny ascii art title."""
+        print(f'{self.name}\n')
+
+        for number, option in self.options.items():
+            print(f"{number}. {option if type(option) is str else option['value']}")
+
+    def add_submenu(self, suboptions, option=1):
+        """Add a submenu with a main option provided."""
+        main_option = self.options[option]
+
+        suboptions.append('Go back')
+
+        # The top-level option is now a dictionary consisting of its value and its submenu.
+        self.options[option] = {
+            'value': main_option,
+            'submenu': dict(enumerate(suboptions, start=1))
+        }
+
+    def show_submenu(self, main_option):
+        """Show a submenu of the provided main option."""
+        if not self.options[main_option]['submenu']:
+            print('This option has no submenu.')
+            return
+
+        for number, option in self.options[main_option]['submenu'].items():
+            print(f"{number}. {option}")

ab6d9eafe34777a652b412a019b294661d290e00
duciito <duciito@gmail.com>
2018-11-03 19:50:51 +0200

    playing around with the api

--- sg/funcs.py
+++ sg/funcs.py
@@ -2,19 +2,28 @@
 
 import requests
 
-snooker_api = dict(
-    event = requests.get('http://api.snooker.org/?e=398'),
-    match = requests.get('http://api.snooker.org/?e=397&r=1&n=5'),
-    player = requests.get('http://api.snooker.org/?p=1'),
-    events_in_season = requests.get('http://api.snooker.org/?t=5&s=2015'),
-    event_matches = requests.get('http://api.snooker.org/?t=6&e=398'),
-    ongoing_matches = requests.get('http://api.snooker.org/?t=7'),
-    player_season_matches = requests.get('http://api.snooker.org/?t=8&p=1&s=2015'),
-    event_players = requests.get('http://api.snooker.org/?t=9&e=403'),
-    pro_players = requests.get('http://api.snooker.org/?t=10&st=p&s=2015'),
-    rankings = requests.get('http://api.snooker.org/?rt=MoneyRankings&s=2015'),
-    round_info = requests.get('http://api.snooker.org/?t=12&e=403')
-)
+# snooker_api = dict(
+#     event = requests.get('http://api.snooker.org/?e=398'),
+#     match = requests.get('http://api.snooker.org/?e=397&r=1&n=5'),
+#     player = requests.get('http://api.snooker.org/?p=1'),
+#     events_in_season = requests.get('http://api.snooker.org/?t=5&s=2015'),
+#     event_matches = requests.get('http://api.snooker.org/?t=6&e=398'),
+#     ongoing_matches = requests.get('http://api.snooker.org/?t=7'),
+#     player_season_matches = requests.get('http://api.snooker.org/?t=8&p=1&s=2015'),
+#     event_players = requests.get('http://api.snooker.org/?t=9&e=403'),
+#     pro_players = requests.get('http://api.snooker.org/?t=10&st=p&s=2015'),
+#     rankings = requests.get('http://api.snooker.org/?rt=MoneyRankings&s=2015'),
+#     round_info = requests.get('http://api.snooker.org/?t=12&e=403')
+# )
 
-for key, value in snooker_api.items():
-    print(f'{key}:   {value.json()}')
+# for key, value in snooker_api.items():
+#     print(f'{key}:   {value.json()}')
+
+events = requests.get('http://api.snooker.org/?t=5&s=2018')
+events = events.json()
+
+event_ids = []
+for event in events:
+    event_ids.append(event['ID'])
+
+print(sorted(event_ids))
